4b408ff623799f1f70418c0a56291be01e7335e1
Sittichoke Som-am (Num) <80870588+numbernard@users.noreply.github.com>
2024-02-16 19:22:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from scipy.stats import linregress
+#from scipy.stats import linregress
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
